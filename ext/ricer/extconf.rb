bf9ffddcbfd1111b1c0414efe1090ab33fe564a2
Charlie Somerville <charlie@charliesomerville.com>
2012-07-27 17:03:27 +1000

    use -fPIC when compiling libuv too

--- ext/ricer/extconf.rb
+++ ext/ricer/extconf.rb
@@ -11,7 +11,7 @@ end
 libuv_dir = File.expand_path("../../libuv", __FILE__)
 ricer_dir = File.expand_path("../", __FILE__)
 
-system "cd '#{libuv_dir}'; make; cd '#{ricer_dir}'; cp #{libuv_dir}/uv.a #{ricer_dir}/libuv.a"
+system "cd '#{libuv_dir}'; CFLAGS='-fPIC' make; cd '#{ricer_dir}'; cp #{libuv_dir}/uv.a #{ricer_dir}/libuv.a"
 
 dir_config "uv", "#{libuv_dir}/include", ricer_dir
 have_library "uv"
